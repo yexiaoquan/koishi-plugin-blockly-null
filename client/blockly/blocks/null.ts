a1baf6f4ff46c72c8272d924d3282999ae80a00a
yexiaoquan <124916709+yexiaoquan@users.noreply.github.com>
2023-12-15 19:56:34 +0800

    a

--- client/blockly/blocks/null.ts
+++ client/blockly/blocks/null.ts
@@ -1,3 +1,4 @@
+import { NAME_SPACE } from "blockly/core/utils/xml";
 import {javascriptGenerator} from "blockly/javascript";
 export const blockly_null = {
 
@@ -10,22 +11,49 @@ export const blockly_null = {
         "text": "空白"
       }
     ],
-    "output": null,
     "colour": 230,
     "tooltip": "啊，没有提示",
     "helpUrl": ""
 
 }
+
 export function blockly_nullBlockGenerator(block){
   let text_name = block.getFieldValue('NAME');
   let code = `${text_name}\n`;
   return code;
 };
 
+export const blockly_null1 = {
+
+  "type": "null1",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "NAME",
+      "text": "空白"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "啊，没有提示",
+  "helpUrl": ""
+
+}
+
+export function blockly_null1BlockGenerator(block, generator) {
+  let text_name = block.getFieldValue('NAME');
+  let code = `${text_name}\n`;
+  return code;
+};
+
 export const nullBlocks = [
-  blockly_null
+  blockly_null,
+  blockly_null1
 ]
 
 export const nullBlockGenerators = {
-  'delete_message':blockly_nullBlockGenerator
+  'null':blockly_nullBlockGenerator,
+  'null1':blockly_null1BlockGenerator
 }
